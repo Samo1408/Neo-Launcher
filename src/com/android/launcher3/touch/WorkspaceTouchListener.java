2fd4b8cdca125e903d1ad6482e914514e04eb709
machiav3lli <antonios.hazim@tutanota.com>
2026-04-19 02:22:42 +0200

    Fix: Avoid showing overview menu on long tap

--- src/com/android/launcher3/touch/WorkspaceTouchListener.java
+++ src/com/android/launcher3/touch/WorkspaceTouchListener.java
@@ -226,7 +226,7 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
                 mWorkspace.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS,
                         HapticFeedbackConstants.FLAG_IGNORE_VIEW_SETTING);
                 mLauncher.getStatsLogManager().logger().log(LAUNCHER_WORKSPACE_LONGPRESS);
-                mLauncher.showDefaultOptions(mTouchDownPoint.x, mTouchDownPoint.y);
+                //mLauncher.showDefaultOptions(mTouchDownPoint.x, mTouchDownPoint.y);
                 mGestureController.onLongPress();
                 if (mLauncher.isSplitSelectionActive()) {
                     mLauncher.dismissSplitSelection(LAUNCHER_SPLIT_SELECTION_EXIT_INTERRUPTED);
